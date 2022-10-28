bc4dedde596d582acaca3cc69edb6044b48290b1
mukeshsin <mukesh@innotechteam.in>
2022-10-28 17:42:54 +0530

    first commit

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+const User = db.users;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+  
+};
+
+
+exports.findAll = (req, res) => {
+  
+};
+
+exports.findOne = (req, res) => {
+  
+};
